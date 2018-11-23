196f2e653153fe636a2669f51e29ba710e1205ef
Mooseymax <mooseandham@gmail.com>
2018-11-23 02:43:04 +0000

    Added in "buy"

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import csv
+from datetime import datetime, timedelta
 from openpyxl import Workbook
 from openpyxl import load_workbook
-from module.fund import Fund
-from module.fund import Transaction
+from module.fund import Fund, Transaction, T_Pool
+
+today = datetime.today()
+example_name = 'Fund A'
+
+example_fund = Fund(1, today, example_name, 100, 1, 1, 100, 100)
+
+if example_fund.f == example_name:
+    print('1. Working')
+
+example_transaction = Transaction([today + timedelta(days=40), 'Fund A2', 'Buy', 100, 2, 200])
+
+example_fund.transact(example_transaction)
+
+print('DEBUG: ' + example_fund.f)
+print('DEBUG: ' + str(example_fund.u))
+print('2. Working')
--- module/fund.py
+++ module/fund.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 class Fund:
-    def __init__(self, key, date, fund_name, units, price, book_price, book_cost, value, gain):
+    def __init__(self, key, date, fund_name, units, price, book_price, book_cost, value):
         self.k = key            # Unique reference
         self.d = date           # Initial date purchased
         self.f = fund_name      # Fund name
@@ -10,8 +10,8 @@ class Fund:
         self.bp = book_price    # Book cost / units
         self.bc = book_cost     # Book cost
         self.v = value          # Current value
-        self.g = gain           # Overall gain -> value - book cost
-        self.t = []             # List of ALL transactions
+        self.g = 0              # Overall gain -> value - book cost
+        self.t = [Transaction([self.d, self.f, 'Initial', self.u, self.p, self.v])]             # List of ALL transactions
         
         ''' 50 unit Buy on 01/01
             50 unit Buy on 01/05
@@ -46,19 +46,28 @@ class Fund:
     
     # Function to realign all previous transactions based on latest transaction
     def update_tran(self, transaction):
+        self.t_pool.a = []  # Same day trades
+        self.t_pool.b = []  # 30 day trades
+        self.t_pool.x = []  # 104 trades
+        
         # 0 / 30 / 104 rule check
-        for transactions in self.t:
-            
+        for t in self.t:
+            print('DEBUG: ' + t.f)
             if t.compare(transaction) == 0:
-                # Do if zero day
+                # Do if 0 day
+                print('DEBUG: Day 0')
+                self.t_pool.a.append(transaction)
             elif t.compare(transaction) == 30:
                 # Do if 30 day
+                self.t_pool.b.append(transaction)
             else:
                 # Do if Section 104
+                self.t_pool.x.append(transaction)
     
-    def tranact(self, transaction):
+    def transact(self, transaction):
         t = transaction
         if t.t == 'Buy':
+            print('DEBUG: Buy')
             self.buy(t)
         elif t.t == 'Sell':
             self.sell(t)
@@ -77,18 +86,40 @@ class Fund:
         
     def buy(self, transaction):
         t = transaction
-        day_a = []  # Same day trades
-        day_b = []  # 30 day trades
-        day_x = []  # 104 trades
+        print('DEBUG: ' + t.f)
         
         # Check and split transactions into separate groups
-        for transactions in self.t:
-            if t.compare(transaction) == 0:
-                day_a.append(transaction)
-            elif t.compare(transaction) == 30:
-                day_b.append(transaction)
-            else:
-                day_x.append(transaction)
+        self.update_tran(t)
+        
+        ''' Loop through to check if matches against any existing pools '''
+        # Do if 0 day not empty and all units not yet matched
+        if self.t_pool.a and not t.check_matched():
+            print('DEBUG: Inside Day 0')
+            for tran_a in self.t_pool.a:
+                t.matched += tran_a.u
+                print("Day 0 rule triggered")
+                if t.check_matched():
+                    break
+        # Do if 30 day not empty and all units not yet matched
+        elif self.t_pool.b and not t.check_matched():
+            for tran_b in self.t_pool.b:
+                t.matched += tran_b.u
+                print("Day 30 rule triggered")
+                if t.check_matched():
+                    break
+        # Do if S104 not empty and all units not yet matched
+        elif self.t_pool.x and not t.check_matched():
+            for tran_x in self.t_pool.x:
+                t.matched += tran_x.u        # Use units in this pool
+                self.u += t.u                   # Add units to fund
+                self.bc += t.v                  # Add cost of purchase
+                self.f = t.f                    # Update fund name (if changed slightly)
+                print("Added to Section 104")   # DEBUG
+                if t.check_matched():
+                    break
+        
+        # Adds transaction to the list
+        self.t.append(transaction)
 
 class T_Pool:
     def __init__(self):
@@ -105,8 +136,17 @@ class Transaction:
         self.p = transaction[4] # Price
         self.v = transaction[5] # Value
         
+        self.matched = 0
+        
         if self.v == None:
             self.v = self.u * self.p    # Value bought
+    
+    # Function to check if matched off against other units in pool
+    def check_matched(self):
+        if self.u == self.matched:
+            return True
+        else:
+            return False
     
     def compare(self, external_transaction):
         t = external_transaction
